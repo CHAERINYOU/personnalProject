26f00a05e3fd67acd90da5ac14764eef584ef6d1
유채린 <cofls000@naver.com>
2023-10-10 17:25:58 +0900

    ..

--- CP/src/main/java/base/board/mapper/BoardMapper.java
+++ CP/src/main/java/base/board/mapper/BoardMapper.java
@@ -1,8 +1,30 @@
 package base.board.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import base.board.Board;
+import base.user.User;
 
 @Mapper
 public interface BoardMapper {
-
+	
+	//게시판 생성
+	public int create(Board board);
+	
+	//게시판 업데이트
+	public int update(int boardno);
+	
+	//게시판 삭제
+	public int remove(int boardno);
+	
+	//게시판 1개 찾기
+	public Board findBoard(int Boardno);
+	
+	//게시판 전체찾기
+	public List<Board> findBoardList();
+	
+	
 }
+	
--- CP/src/main/java/base/product/ProductDaoImpl.java
+++ CP/src/main/java/base/product/ProductDaoImpl.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 @Repository(value = "productDaoImplMyBatisMapperInterface")
 public class ProductDaoImpl {
 
-	private P
+
 }
